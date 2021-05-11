1eaaa9ceee875b9b931ad8af0ed0de9d5addb142
mengguo <tony2015116@163.com>
2021-05-11 15:13:15 +0800

    fi_plot() can plot fire station data now

--- R/fi_plot.R
+++ R/fi_plot.R
@@ -9,6 +9,11 @@
 #' #fi_plot(data = test)
 fi_plot <- function(data) {
   feed_intake <- NULL
+  if(names(data)[8] == "Consumed"){
+    names(data)[8] <- "feed_intake"
+    data <- data %>%
+      dplyr::mutate(feed_intake = 1000 * feed_intake)
+  }
   ggplot2::ggplot(data, ggplot2::aes(feed_intake)) +
     ggplot2::geom_histogram(binwidth = 8) +
     ggplot2::theme_bw()
